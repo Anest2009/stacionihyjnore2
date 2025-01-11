8f61f864ea7a2d91014812344686bfa6b7e28345
Anest <103149273+Anest2009@users.noreply.github.com>
2025-01-11 17:22:25 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+    target: 'esnext'
+  },
+  server: {
+    port: 3000
+  }
 });
